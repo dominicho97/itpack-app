74f1389b83d72eca5b72a5de7d6aa0b8317987a8
dominicho97 <dominic.ho97@gmail.com>
2019-06-20 17:08:50 +0200

    added input

--- creat-account.php
+++ creat-account.php
@@ -8,3 +8,10 @@ $pdo>setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 ?>
+
+<h1>Register</h1>
+<form class="create-account.php" action="index.html" method="post" >
+<input type='text' name="username" value="" placeholder="Username..">
+<input type='password' name="password" value="" placeholder="Password..">
+<input type='submit' name='createaccount' value="Create Account">
+</form>
