ab247f09f4347d25b6a1e5ad28429d2bed92c001
dominicho97 <dominic.ho97@gmail.com>
2019-06-20 18:04:42 +0200

    added if isset..

--- create-account.php
+++ create-account.php
@@ -4,15 +4,18 @@
 $pdo = new PDO('mysql:host=localhost;dbname=itpack;charset=utf8', 'root','' );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+if (isset($_POST['createaccount'])){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+}
 
 
 ?>
 
 <h1>Register</h1>
 <form action="create-account.php" method="post">
-<input type="text" name="username" value="" placeholder="Username ..."><p />
-<input type="password" name="password" value="" placeholder="Password ..."><p />
+<input type="text" name="username" value="" placeholder="Username ..."></p >
+<input type="password" name="password" value="" placeholder="Password ..."></p >
 
 <input type="submit" name="createaccount" value="Create Account">
 </form>
