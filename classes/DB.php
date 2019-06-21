63e76f9ed097edbba66603d71491184e56b9772c
dominicho97 <dominic.ho97@gmail.com>
2019-06-21 14:51:59 +0200

    added return data

--- classes/DB.php
+++ classes/DB.php
@@ -4,5 +4,16 @@ class DB {
   private static function connect(){
     $pdo = new PDO('mysql:host=localhost;dbname=itpack;charset=utf8', 'root','' );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
   }
+
+  public static function query($query,$params){
+    $statement = self::connect()->prepare($query);
+    $statement->execute();
+    $data =  $statement->fetchAll();
+    return $data;
+  }
+
 }
+
+
