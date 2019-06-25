1c742229e94c7f9536a403fde85f0ba856cd51eb
dominicho97 <dominic.ho97@gmail.com>
2019-06-25 19:34:20 +0200

    added logout file and form

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+include('./classes/DB.php');
+include('./classes/Login.php');
+?>
+
+
+<h1>Logout of your Account?</h1>
+<p>Are you sure you'd like to logout?</p>
+<form action="logout.php" method="post">
+        <input type="checkbox" name="alldevices" value="alldevices"> Logout of all devices?<br />
+        <input type="submit" name="confirm" value="Confirm">
+</form>
